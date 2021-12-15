112e9f1898a599137c0ef4b3a3baeb5d33a7ac2c
GustavoPercastre <angelpercastre293@gmail.com>
2021-12-15 05:14:27 -0800

    Game active will store the current player to keep track of who's turn it is. Current player will will store the current game state. And game state will store the messages for the game.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const statusDisplay = document.querySelector('.game--status');
 // This will keep track of who's turn it is.
+
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+// Game active will store the current player to keep track of who's turn it is. Current player will will store the current game state. And game state will store the messages for the game. 
+
+
+
+
