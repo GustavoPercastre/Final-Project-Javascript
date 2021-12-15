883dc6c62509bbfbfe03bc1c63efc078ce5524b0
GustavoPercastre <angelpercastre293@gmail.com>
2021-12-15 05:25:05 -0800

    These are the win conditions in tic tac toe.

--- script.js
+++ script.js
@@ -14,4 +14,17 @@ const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 statusDisplay.innerHTML = currentPlayerTurn();
 // This will display the current turn.
 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+// These are the win conditions in tic tac toe.
+
+
 
