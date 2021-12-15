c75120161d9d0d6b9d81b74146efbef0b3285e41
GustavoPercastre <angelpercastre293@gmail.com>
2021-12-15 05:17:08 -0800

    These messages will go into the game status h2.

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 // Game active will store the current player to keep track of who's turn it is. Current player will will store the current game state. And game state will store the messages for the game. 
 
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+// These messages will go into the game status h2.
+
 
 
 
