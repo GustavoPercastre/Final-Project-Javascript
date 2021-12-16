0df2bc76cc1a12aa96d303884330b73fd5cbc7c4
GustavoPercastre <angelpercastre293@gmail.com>
2021-12-15 21:40:36 -0800

    These will allow for the scoreboard to keep count of the x and o scores.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const statusDisplay = document.querySelector('.game--status');
 // This will keep track of who's turn it is.
 
+const statusScoreX = document.querySelector('#scoreboard-x');
+const statusScoreO = document.querySelector('#scoreboard-o');
+//These will allow for the scoreboard to keep count of the x and o scores
+
 let gameActive = true;
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
 // Game active will store the current player to keep track of who's turn it is. Current player will will store the current game state. And game state will store the messages for the game. 
+
+
 
 const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
